510e8e697d26bcbb1b77b737ade0aad6a75b5a6e
Matthew Phillips <phillipsm2@gmail.com>
2019-11-24 16:32:30 -0500

    Some progress on this branch

--- test/test.js
+++ test/test.js
@@ -1,8 +1,8 @@
 const compile = require('../lib/index.js');
 
 async function example() {
-  let src = await compile(__dirname + '/input/static-helloworld.html');
-  console.log(src);
+  let result = await compile(__dirname + '/input/counter.html');
+  console.log(result.html);
 }
 
 example();
